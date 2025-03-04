17fa32bdb3c8dc2d48b6213aee81d5f21f701fd5
brijesh-2001 <Brijeshrawat2001@gmail.com>
2025-03-04 12:44:29 +0530

    changes

--- backend/connection/conn.js
+++ backend/connection/conn.js
@@ -2,13 +2,13 @@ const mongoose=require("mongoose");
 
 const conn=async (req,res)=>{
     try {
-        await mongoose.connect("mongodb+srv://dummy:dummy@cluster0.4n3jg.mongodb.net/")
+        await mongoose.connect("mongodb+srv://brijeshrawat2001:brijesh@cluster0.ksdv2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     .then(()=>{
         console.log("connected");
     });
     } catch (error) {
         res.status(400).json({
-            message:"Not Connected",
+            message:("Not Connected"),
         });
     }
 };
